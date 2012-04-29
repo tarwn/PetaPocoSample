21466786f75b912e9d85c2fc3477cf966fa5f5d2
Eli Weinstock-Herman <tarwn@tiernok.com>
2012-04-29 10:00:40 -0400

    switch to SQL so I can use profiler

--- PetaPocoSample/Program.cs
+++ PetaPocoSample/Program.cs
@@ -10,11 +10,15 @@ namespace PetaPocoSample {
 	class Program {
 		static void Main(string[] args) {
 			var p = new Program();
-			p.CreateDatabase();
-			p.CreateTable();
+			//p.CreateDatabase();
+			//p.CreateTable();
 			p.CreatePerson();
 			p.CreatePerson2();
 			p.CreateDecoratedPerson();
+			Console.Read();
+			p.SelectRecords();
+			Console.Read();
+			p.SelectDecoratedRecords();
 
 			Console.WriteLine("\nPress enter to continue");
 			Console.Read();
@@ -36,13 +40,15 @@ namespace PetaPocoSample {
 		}
 
 		private Database GetDatabase() {
-			return new Database("DataSource=\"test.sdf\"; Password=\"chrissiespassword\"", "System.Data.SqlServerCe.4.0");
+//			return new Database("DataSource=\"test.sdf\"; Password=\"chrissiespassword\"", "System.Data.SqlServerCe.4.0");
+			return new Database("Data Source=localhost;Initial Catalog=SampleStuff;Integrated Security=SSPI;", "System.Data.SqlClient");
 		}
 
 		private void OutputPersonTable(string name) {
 			Console.WriteLine("\n" + name);
 			using (var db = GetDatabase()) {
 				var persons = db.Query<Person>("SELECT * FROM Person;");
+				Console.WriteLine(db.LastSQL);
 				Console.WriteLine(String.Join("\n", persons.Select(p => p.ToString())));
 			}
 		}
@@ -57,7 +63,7 @@ namespace PetaPocoSample {
 
 		private void CreatePerson2() {
 			using (var db = GetDatabase()) {
-				db.Insert(new Person() { LastName = "lastname1", FirstName = "firstname1" });
+				db.Insert(new Person() { LastName = "lastname2", FirstName = "firstname2" });
 			}
 
 			OutputPersonTable("CreatePerson2");
@@ -65,10 +71,24 @@ namespace PetaPocoSample {
 
 		private void CreateDecoratedPerson() {
 			using (var db = GetDatabase()) {
-				db.Insert(new DecoratedPerson() { LastName = "lastname1", FirstName = "firstname1" });
+				db.Insert(new DecoratedPerson() { LastName = "lastname3", FirstName = "firstname3" });
 			}
 
 			OutputPersonTable("CreateDecoratedPerson");
+		}
+
+		private void SelectRecords() {
+			using (var db = GetDatabase()) {
+				var results = db.Query<Person>("SELECT * FROM Person WHERE lastname=@0", "lastname1");
+				Console.WriteLine(String.Join("\n", results.Select(p => p.ToString())));
+			}
+		}
+
+		private void SelectDecoratedRecords() {
+			using (var db = GetDatabase()) {
+				var results = db.Query<DecoratedPerson>("WHERE lastname=@0", "lastname1");
+				Console.WriteLine(String.Join("\n", results.Select(p => p.ToString())));
+			}
 		}
 	}
 
